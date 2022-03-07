73813b085b983d3cfda7b42a130fa6c92b23baf7
MarsadMaqsood <marsadmaqsood149@gmail.com>
2022-03-07 23:17:50 +0500

    updated

---
+++ lib/src/controller.dart
@@ -0,0 +1,13 @@
+import 'package:stylish_dialog/stylish_dialog.dart';
+
+abstract class Controller {
+  void setValue(DialogStatus status);
+
+  Function(DialogStatus status)? listener;
+
+  void dismiss() {}
+
+  void dispose();
+
+  ///...
+}
