0af7954e3ebd48f9c5c0f2446f73a67161d3c4d3
MarsadMaqsood <marsadmaqsood149@gmail.com>
2022-04-30 11:33:06 +0500

    updated

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:stylish_dialog/stylish_dialog.dart';
 
-abstract class Controller {
-  void setValue(DialogStatus status);
+abstract class Controller extends ChangeNotifier {
+  static setValue(DialogStatus status) {}
 
-  Function(DialogStatus status)? listener;
+  void Function(DialogStatus status)? listener;
 
   void dismiss();
 
+  @override
   void dispose();
 }
